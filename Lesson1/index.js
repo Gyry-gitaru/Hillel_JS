b5787bf9b906743838cde0e53bc32b6c0ec6ea26
Yana <chelyshevayana@gmail.com>
2022-06-17 12:24:07 +0100

    add lesson 1

---
+++ Lesson1/index.js
@@ -0,0 +1,14 @@
+const userName = prompt('Please enter your name?');
+const userSurname = prompt('Please enter your surname?');
+
+alert('Hi,' + userName + ' ' + userSurname);
+
+const userAge = prompt('How are you old?');
+const userHobby = prompt('What is your hobby?');
+
+alert('Nice to meet you');
+
+console.log(userName, userSurname, userAge, userHobby);
+
+
+
